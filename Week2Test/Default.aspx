0d7132e380dd09d453e7c5b2cb95367387bfeea9
11927 <11927@C10PC32>
2025-06-05 13:11:00 +1000

    Add project files.

---
+++ Week2Test/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Week2Test.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:Label ID="Label1" runat="server" Text="This is a label"></asp:Label>
+            <br />
+            <asp:GridView ID="gvResult" runat="server"></asp:GridView>
+        </div>
+       
+    </form>
+</body>
+</html>
---
+++ Week2Test/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Week2Test
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //  lblTitle.Text = " I'm losing my mind";
+
+            //1. Open the connection
+            String _connectionString = "Data Source=SQL5110.site4now.net;Initial Catalog=db_9ab8b7_25dda11927;User Id=db_9ab8b7_25dda11927_admin;Password=dSZm274x;";
+
+            //Create connection
+            SqlConnection conn = new SqlConnection();
+           conn.ConnectionString = _connectionString;
+            conn.Open();
+
+            //2. Prepare consume intsruction
+            SqlCommand command = new SqlCommand("SELECT * FROM TAbUser", conn);
+
+            //3. Consume
+            SqlDataReader reader = command.ExecuteReader();
+
+            //4. Prepare storage 
+            DataTable dt = new DataTable();
+            dt.Columns.Add("UserID", typeof(Int32));
+            dt.Columns.Add("UserName", typeof(String));
+            dt.Columns.Add("UserLevel", typeof(Int32));
+
+            //5.Perform consumption
+            while (reader.Read())
+            {
+                DataRow row = dt.NewRow();
+                row["UserID"] = reader["UID"];
+                row["UserName"] = reader["UserName"];
+                row["USerLevel"] = reader["USerLevel"];
+
+                dt.Rows.Add(row);
+            }
+
+            //6. Display
+            gvResult.DataSource = dt;
+            gvResult.DataBind();
+
+            //7. Close connection
+            conn.Close();
+        }
+    }
+}
