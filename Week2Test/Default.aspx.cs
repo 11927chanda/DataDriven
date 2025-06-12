0d8e7bd55eb36a46b982badf57f9d085b0211192
11927 <11927@C10PC32>
2025-06-12 11:39:38 +1000

    error handeling

--- Week2Test/Default.aspx.cs
+++ Week2Test/Default.aspx.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Week2Test
 {
@@ -18,7 +13,7 @@ namespace Week2Test
 
             //1. Open the connection
             String _connectionString = "";
-            if(ConfigurationManager.ConnectionStrings["DevelopmentConnectiontring"].ConnectionString.Equals("Dev"))
+            if (ConfigurationManager.ConnectionStrings["DevelopmentConnectiontring"].ConnectionString.Equals("Dev"))
             {
                 _connectionString = AppConstant.AppConnection.DevConnection;
 
@@ -36,39 +31,59 @@ namespace Week2Test
 
 
             //Create connection
+
             SqlConnection conn = new SqlConnection();
-           conn.ConnectionString = _connectionString;
-            conn.Open();
-
-            //2. Prepare consume intsruction
-            SqlCommand command = new SqlCommand("SELECT * FROM TAbUser", conn);
-
-            //3. Consume
-            SqlDataReader reader = command.ExecuteReader();
-
-            //4. Prepare storage 
-            DataTable dt = new DataTable();
-            dt.Columns.Add("UserID", typeof(Int32));
-            dt.Columns.Add("UserName", typeof(String));
-            dt.Columns.Add("UserLevel", typeof(Int32));
-
-            //5.Perform consumption
-            while (reader.Read())
+            try
             {
-                DataRow row = dt.NewRow();
-                row["UserID"] = reader["UID"];
-                row["UserName"] = reader["UserName"];
-                row["UserLevel"] = reader["UserLevel"];
+                conn.ConnectionString = _connectionString;
+                conn.Open();
 
-                dt.Rows.Add(row);
+                //2. Prepare consume intsruction
+                SqlCommand command = new SqlCommand("SELECT * FROM TAbUser", conn);
+
+                //3. Consume
+                SqlDataReader reader = command.ExecuteReader();
+
+                //4. Prepare storage 
+                DataTable dt = new DataTable();
+                dt.Columns.Add("UserID", typeof(Int32));
+                dt.Columns.Add("UserName", typeof(String));
+                dt.Columns.Add("UserLevel", typeof(Int32));
+
+                //5.Perform consumption
+                while (reader.Read())
+                {
+                    DataRow row = dt.NewRow();
+                    row["UserID"] = reader["UID"];
+                    row["UserName"] = reader["UserName"];
+                    row["UserLevel"] = reader["UserLevel"];
+
+                    dt.Rows.Add(row);
+                }
+
+                //6. Display
+                gvResult.DataSource = dt;
+                gvResult.DataBind();
+
+                //7. Close connection
+                conn.Close();
             }
-
-            //6. Display
-            gvResult.DataSource = dt;
-            gvResult.DataBind();
-
-            //7. Close connection
-            conn.Close();
+            catch (InvalidOperationException ex)
+            {
+                //
+            }
+            catch (SqlException ex)
+            {
+                //
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                //
+            }
+            catch (Exception ex)
+            {
+                //
+            }
         }
     }
 }
